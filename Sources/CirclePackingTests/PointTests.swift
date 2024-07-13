7dd7a710841ee8e966228871b12d7ac666fd660f
Liza Osipova <lizaosipova@list.ru>
2024-07-13 17:04:22 +0300

    Add *=, /= tests

--- Sources/CirclePackingTests/PointTests.swift
+++ Sources/CirclePackingTests/PointTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class PointTests: XCTestCase {
     func testOperators() {
-        let a = Point(x: 10, y: 10)
+        var a = Point(x: 10, y: 10)
         let b = Point(x: 2, y: 3)
         XCTAssertEqual(a + b, Point(x: 12, y: 13))
         XCTAssertEqual(a - b, Point(x: 8, y: 7))
@@ -14,5 +14,9 @@ class PointTests: XCTestCase {
         XCTAssertEqual(-c, Point(x: -4, y: -6))
         c -= Point(x: 5, y: 6)
         XCTAssertEqual(c, Point(x: -1, y: 0))
+        a *= 2
+        XCTAssertEqual(a, Point(x: 20, y: 20))
+        a /= 2
+        XCTAssertEqual(a, Point(x: 5, y: 5))
     }
 }
