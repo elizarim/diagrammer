59ebdb4dd4a85a1d044e4df11b5501dc0155210b
Liza Osipova <lizaosipova@list.ru>
2024-09-01 16:06:28 +0300

    Update download link

--- higapp.rb
+++ higapp.rb
@@ -1,7 +1,7 @@
 class HigApp < Formula
   desc "Generates image with hierarchy diagram using nested circles."
   homepage "https://github.com/elizarim/higapp"
-  url "https://github.com/elizarim/higapp/archive/higapp-0.0.1.tar.gz"
+  url "https://github.com/elizarim/higapp/releases/download/0.0.1/higapp-0.0.1.tar.gz"
   sha256 "567396b7259979f835ed74af29267ff63bfa5a308530272cdf4c0a3e965f14d9"
 
   depends_on xcode: ["15"]
